2837e557dde1bbdda3d8f0d6862cc123bd8660a9
ANIR1604 <competitivecoder69@gmail.com>
2025-05-03 00:31:56 +0530

    BE Connected

--- components/loading-analysis.tsx
+++ components/loading-analysis.tsx
@@ -7,7 +7,7 @@ interface LoadingAnalysisProps {
 export function LoadingAnalysis({ step }: LoadingAnalysisProps) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#0a0a1f] via-[#1a103c] to-[#2d1b4e]">
-      {/* <ProcessingAnimation currentStep={step} /> */}
+      <ProcessingAnimation currentStep={step} />
     </div>
   );
 } 
